20db25eb516ecdcc55e8460f86dde1c73c1160da
arnauddegez <adegez@gmail.com>
2021-06-16 11:37:58 +0200

    update script

--- script-arnaud.sh
+++ script-arnaud.sh
@@ -18,14 +18,18 @@ dirbackup=/backup/backup-$backupdate
 # sauvegarde mysql
 /usr/bin/mysqldump --user=xxxx --password=xxxx --all-databases | /usr/bin/gzip > $dirbackup/mysqldump-$backupdate.sql.gz
 
+#importation des ID de connexion sur aws
 aws configure import --csv file://credentials.csv
 
+#creation du bucket 
 aws s3api create-bucket --bucket bucketdearnaud --region eu-west-1 --create-bucket-configuration LocationConstraint=eu-west-1
 
+# copie des fichier de backup 
 aws s3 cp home-$backupdate.tar.bz2 s3://bucketdearnaud/backup
 
 aws s3 cp mysqldump-$backupdate.sql.gz s3://bucketdearnaud/backup
 
+#mise en place du cycle de vie des fichiers pour les supprimer au bout de 7 jours
 aws s3api put-bucket-lifecycle-configuration  \
 --bucket bucketdearnaud  \
 --lifecycle-configuration file://lifecycle.json
